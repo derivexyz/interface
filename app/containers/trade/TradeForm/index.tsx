87a38475207b38b27d615972c0151ba9248c74c6
Dillon Lin <30581721+DillonLin@users.noreply.github.com>
2022-11-08 16:20:19 +1100

    fix: temporary increase slippage

--- app/containers/trade/TradeForm/index.tsx
+++ app/containers/trade/TradeForm/index.tsx
@@ -41,7 +41,7 @@ type Props = {
   onTrade?: (market: Market, positionId: number) => void
 }
 
-const SLIPPAGE = 0.5 / 100 // 0.5%
+const SLIPPAGE = 5 / 100 // 5.0%
 
 const TradeForm = withSuspense(
   ({ isBuy, option, positionId, onTrade, hideTitle }: Props) => {
