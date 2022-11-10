08e8dd32205cd948c5f689cfd312b1e5ae038156
Michael Wu <87842872+michaelxuwu@users.noreply.github.com>
2022-11-10 13:40:36 +1100

    fix: transaction screening for no accountt

--- app/hooks/wallet/useScreenTransaction.ts
+++ app/hooks/wallet/useScreenTransaction.ts
@@ -21,17 +21,16 @@ const fetcher = async (address: string, transactionType: TransactionType): Promi
   return data
 }
 
+const DEFAULT_RESPONSE: ScreenTransactionData = {
+  isBlocked: false,
+  blockReason: null,
+  blockDescription: null,
+}
+
 export default function useScreenTransaction(transactionType: TransactionType): ScreenTransactionData | null {
-  const { account } = useWallet()
+  const { connectedAccount: account } = useWallet()
   const isReady = useIsReady()
   const isScreenable = isReady && isScreeningEnabled()
   const [data] = useFetch('ScreenTransaction', isScreenable && account ? [account, transactionType] : null, fetcher)
-  if (!isOptimismMainnet()) {
-    return {
-      isBlocked: false,
-      blockReason: null,
-      blockDescription: null,
-    }
-  }
-  return data
+  return isScreenable && account && isOptimismMainnet() ? data : DEFAULT_RESPONSE
 }
