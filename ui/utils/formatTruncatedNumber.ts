3fb8b6b170f33a3c1ed91cb82c359adc4bb6dbf6
Dillon Lin <30581721+DillonLin@users.noreply.github.com>
2022-12-15 17:49:47 +1100

    fix: truncated numbers

--- ui/utils/formatTruncatedNumber.ts
+++ ui/utils/formatTruncatedNumber.ts
@@ -3,9 +3,9 @@ import { BigNumber } from '@ethersproject/bignumber'
 import formatNumber from './formatNumber'
 import fromWei from './fromWei'
 
-const BILLION = Math.pow(10, 9) - Math.pow(10, 7)
-const MILLION = Math.pow(10, 6) - Math.pow(10, 4)
-const THOUSAND = Math.pow(10, 3) - Math.pow(10, 1)
+const BILLION = Math.pow(10, 9)
+const MILLION = Math.pow(10, 6)
+const THOUSAND = Math.pow(10, 3)
 
 export default function formatTruncatedNumber(value: BigNumber | number) {
   let val = 0
@@ -15,13 +15,13 @@ export default function formatTruncatedNumber(value: BigNumber | number) {
     val = value
   }
   // TODO: @dappbeast Add trillion case... one day 8)
-  if (Math.abs(val) >= BILLION) {
+  if (Math.abs(val) >= BILLION - Math.pow(10, 7)) {
     // billion
     return formatNumber(val / BILLION, { minDps: 0, maxDps: 2 }) + 'b'
-  } else if (Math.abs(val) >= MILLION) {
+  } else if (Math.abs(val) >= MILLION - Math.pow(10, 4)) {
     // million
     return formatNumber(val / MILLION, { minDps: 0, maxDps: 2 }) + 'm'
-  } else if (Math.abs(val) >= THOUSAND) {
+  } else if (Math.abs(val) >= THOUSAND - Math.pow(10, 1)) {
     // thousand
     return formatNumber(val / THOUSAND, { minDps: 0, maxDps: 2 }) + 'k'
   } else {
