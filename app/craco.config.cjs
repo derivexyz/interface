1d7952b300b2e4a33d0277d8662e030a557ec067
Michael Wu <87842872+michaelxuwu@users.noreply.github.com>
2022-12-16 13:50:12 +1100

    fix official wallet connect modal (#158)

--- app/craco.config.cjs
+++ app/craco.config.cjs
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const { getLoader, loaderByName } = require('@craco/craco')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
@@ -22,6 +23,14 @@ module.exports = {
         webpackConfig.plugins.push(
           new BundleAnalyzerPlugin({ analyzerMode: process.env.REACT_APP_INTERACTIVE_ANALYZE ? 'server' : 'json' })
         )
+      }
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+        })
+      )
+      webpackConfig.resolve.fallback = {
+        buffer: require.resolve('buffer'),
       }
 
       return webpackConfig
