a47e7964f257b2bf33f1560fc924dc8907c52a00
dappbeast <jake@lyra.finance>
2023-01-23 08:59:01 +1100

    fix: hide default network

--- app/src/utils/useDefaultNetwork.ts
+++ app/src/utils/useDefaultNetwork.ts
@@ -4,9 +4,13 @@ import nullthrows from 'nullthrows'
 import { LOCAL_STORAGE_DEFAULT_NETWORK_KEY } from '../constants/localStorage'
 import useLocalStorage from '../hooks/local_storage/useLocalStorage'
 import coerce from './coerce'
+import isProd from './isProd'
 
 export default function useDefaultNetwork(): Network {
   const [defaultNetworkLocalRaw, setDefaultNetwork] = useLocalStorage(LOCAL_STORAGE_DEFAULT_NETWORK_KEY)
+  if (isProd()) {
+    return Network.Optimism
+  }
   const defaultNetworkLocal = coerce(Network, defaultNetworkLocalRaw)
   if (!defaultNetworkLocal) {
     const defaultNetworkEnv = nullthrows(
