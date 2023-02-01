9cae883c02298c7c9d865c529353a8254e08cb92
dappbeast <jake@lyra.finance>
2023-02-01 11:42:26 +1100

    feat: newport launch

--- app/src/utils/useDefaultNetwork.ts
+++ app/src/utils/useDefaultNetwork.ts
@@ -6,14 +6,13 @@ import useLocalStorage from '../hooks/local_storage/useLocalStorage'
 import coerce from './coerce'
 
 export default function useDefaultNetwork(): Network {
-  const [defaultNetworkLocalRaw, setDefaultNetwork] = useLocalStorage(LOCAL_STORAGE_DEFAULT_NETWORK_KEY)
+  const [defaultNetworkLocalRaw] = useLocalStorage(LOCAL_STORAGE_DEFAULT_NETWORK_KEY)
   const defaultNetworkLocal = coerce(Network, defaultNetworkLocalRaw)
   if (!defaultNetworkLocal) {
     const defaultNetworkEnv = nullthrows(
       coerce(Network, process.env.REACT_APP_DEFAULT_NETWORK),
       'REACT_APP_DEFAULT_NETWORK is not defined'
     )
-    setDefaultNetwork(defaultNetworkEnv)
     return defaultNetworkEnv
   } else {
     return defaultNetworkLocal
