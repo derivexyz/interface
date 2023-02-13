01a47ec2714c2864ae87b648928e1f9b6805975e
Michael Wu <87842872+michaelxuwu@users.noreply.github.com>
2023-02-13 11:09:53 +1100

    fix: pending staking rewards

--- app/src/containers/rewards/RewardsBreakdownCard/PendingRewardsCardSection.tsx
+++ app/src/containers/rewards/RewardsBreakdownCard/PendingRewardsCardSection.tsx
@@ -39,12 +39,10 @@ const PendingRewardsCardGridItems = withSuspense(
     const wethLyraStakingL2Rewards = findOpRewardEpochToken(account?.wethLyraStakingL2.rewards ?? [])
     const opRewards =
       opStakingRewards + opVaultRewards + opTradingRewards + opShortCollatRewards + wethLyraStakingL2Rewards
-    const stkLyraStakingRewards = findLyraRewardEpochToken(account?.stakingRewards ?? [])
     const stkLyraVaultRewards = findLyraRewardEpochToken(account?.totalVaultRewards ?? [])
     const stkLyraTradingRewards = findLyraRewardEpochToken(account?.tradingRewards ?? [])
     const stkLyraShortCollatRewards = findLyraRewardEpochToken(account?.shortCollateralRewards ?? [])
-    const stkLyraRewards =
-      stkLyraStakingRewards + stkLyraVaultRewards + stkLyraTradingRewards + stkLyraShortCollatRewards
+    const stkLyraRewards = stkLyraVaultRewards + stkLyraTradingRewards + stkLyraShortCollatRewards
 
     // TODO: @dillon remove flags later
     const isDepositPeriod = global?.isDepositPeriod
@@ -91,7 +89,6 @@ const PendingStakedLyraText = withSuspense(
     const account = epochs?.account
     // TODO: @dillon remove next epoch
     const isDepositPeriod = epochs?.global.isDepositPeriod
-    // Omit staking rewards due to 6mo lock
     const lyraVaultRewards = findLyraRewardEpochToken(account?.totalVaultRewards ?? [])
     const lyraTradingRewards = findLyraRewardEpochToken(account?.tradingRewards ?? [])
     const lyraShortCollatRewards = findLyraRewardEpochToken(account?.shortCollateralRewards ?? [])
