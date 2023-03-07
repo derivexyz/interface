2cb68f2f2ab4b9b2df8cb4486e07dfd04b0f19ee
Игорь Фонин <menotoa@gmail.com>
2023-03-07 09:40:56 +0300

    fix: explorer url
    remove double slash from explorer link

--- app/src/utils/getExplorerUrl.ts
+++ app/src/utils/getExplorerUrl.ts
@@ -12,5 +12,5 @@ export default function getExplorerUrl(network: Network | 'ethereum', transactio
       : getNetworkConfig(getChainForChainId(getChainIdForNetwork(network)))
   const explorerUrl = networkConfig.blockExplorerUrl
   const type = transactionHashOrAddress.length > 42 ? 'tx' : 'address'
-  return `${explorerUrl}/${type}/${transactionHashOrAddress}`
+  return `${explorerUrl}${type}/${transactionHashOrAddress}`
 }
