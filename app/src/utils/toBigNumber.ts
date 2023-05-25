3d92791a8274729ebc29b95b1ff89e3794b87d8f
Dillon Lin <30581721+DillonLin@users.noreply.github.com>
2023-05-25 17:13:43 +1000

    fix: toBigNumber

--- app/src/utils/toBigNumber.ts
+++ app/src/utils/toBigNumber.ts
@@ -2,6 +2,5 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { parseUnits } from '@ethersproject/units'
 
 export default function toBigNumber(number: number, decimals: number = 18): BigNumber {
-  const numberStr = number.toString().includes('e') ? number.toFixed(decimals).toString() : number.toString()
-  return parseUnits(numberStr, decimals)
+  return parseUnits(number.toFixed(decimals).toString(), decimals)
 }
